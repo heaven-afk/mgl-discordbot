e95be2cd53b540e7fdf4ffd25a6d3b034c4d2c08
Ogadi Zion <ogadizion01@gmail.com>
2026-02-25 22:53:53 +0100

    Add Render deployment config and fix keepAlive port

--- src/keepAlive.js
+++ src/keepAlive.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Simple health check endpoint
 app.get('/', (req, res) => {
